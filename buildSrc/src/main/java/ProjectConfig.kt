7eca26f46899b924f7b78bae0d2cbe6f9b9ca18a
nikhileshsc <86941879+nikhileshsc@users.noreply.github.com>
2026-09-22 14:39:25 +0530

    Bump versionCode 63 -> 64 for Billing Library 9 release

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,7 +2,7 @@ object ProjectConfig {
     const val compileSdkVersion = 36
     const val minSdkVersion = 24
     const val targetSdkVersion = 36//34
-    const val versionCode = 63
+    const val versionCode = 64
     const val versionName = "2.2.1"
     const val applicationId = "com.companion.astrodating"
 }
